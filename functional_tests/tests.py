727532e857cc397710ae4890327c83f23c89343b
qizhihuanying <qzhycloud@outlook.com>
2024-05-09 23:21:06 +0800

    message 20: testing for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,3 +87,22 @@ class NewVisitorTest(LiveServerTestCase):
         
         # self.fail('Finish the test!')
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            622,
+            delta=10
+            )
+        inputbox.sendkeys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            622,
+            delta=10
+        )
