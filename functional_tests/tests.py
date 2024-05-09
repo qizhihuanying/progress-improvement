46148b10a98733758408c7adb2429bfce6a204da
qizhihuanying <qzhycloud@outlook.com>
2024-05-09 15:09:23 +0800

    message 13 : refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,9 @@ import time
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
  
 class NewVisitorTest(LiveServerTestCase):
     
@@ -13,10 +16,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+             
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         
@@ -30,20 +42,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-        )
-        
+        )      
         inputbox.send_keys("Buy flowers")  
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy flowers')  
+        self.wait_for_row_in_list_table('1: Buy flowers')
               
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')     
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         
         
         self.fail('Finish the test!')
